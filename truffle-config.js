513672dd0c493c7aec6e49db53b3d1b856abe873
Raditya Perdhevi <raditya@perdhevi.com>
2021-08-22 15:27:29 +0700

    fix:truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -22,11 +22,14 @@
  *
  */
 
-// const HDWallet = require('truffle-hdwallet-provider');
+ 
+
+const HDWallet = require('truffle-hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
 // const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+ //const mnemonic = fs.readFileSync(".secret").toString().trim();
+ const mnemonic = process.env.RINKEBY_MNEMONIC;
 
 module.exports = {
   /**
@@ -72,6 +75,11 @@ module.exports = {
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
+
+    rinkeby:{
+      provider: () => new HDWallet(mnemonic, 'https://rinkeby.infura.io/v3/ef2776eb59ee4eb0ac2e5e5a71c37ece'),
+      network_id: 4,
+    }
 
     // Useful for private networks
     // private: {
